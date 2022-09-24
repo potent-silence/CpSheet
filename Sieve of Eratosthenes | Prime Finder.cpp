55a7a99f854c1705eecb8b61f642d063b97c9740
potent-silence <75715464+potent-silence@users.noreply.github.com>
2022-09-24 15:59:35 +0530

    Sieve of Eratosthenes
    Used to find Prime Numbers on nth no.
    In input if you gave a no. r the output gives a prime no. who is ranked r if all prime no.s are arranged in ascending order

--- Sieve of Eratosthenes | Prime Finder.cpp
+++ Sieve of Eratosthenes | Prime Finder.cpp
@@ -7,14 +7,14 @@ int main() {
     is_prime[0] = is_prime[1] = false;
     for (int i = 2; i * i <= n; i++) {
         if (is_prime[i]) {
-            for (int j = i * i; j <= n; j += i)
-                is_prime[j] = false;
+            for (int j = i * i; j <= n; j += i)//taking no.s from taken no.s square to the last no.
+                is_prime[j] = false;           //making multiples of the taken no. 0 in declare is_prime array
         }   
     }
     vector<int>primes;
     for (int i = 2; i<= n; i++) {
         if(is_prime[i]){
-            primes.push_back(i);
+            primes.push_back(i);//Primes are added
         }
     }
     int test,pos;
