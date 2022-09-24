a21b8fdfbebbb270627d5767822c480e73f260f5
potent-silence <75715464+potent-silence@users.noreply.github.com>
2022-09-24 16:22:15 +0530

    Vector without test cases

---
+++ vectorwithouttest.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long unsigned int llu;
+typedef long long int lli;
+
+int main() {
+   long long int n,i;
+   cin>>n;
+   vector<long long int>arr;
+   for(i=0;i<n;i++){
+     lli temp;
+     cin>>temp;
+     arr.push_back(temp);//arr.emplace_back(temp);
+   }
+   //code goes here;
+  return 0;
+}
