08bc8a4a8113b4468786efa9ed9bd92c3f1750a3
potent-silence <75715464+potent-silence@users.noreply.github.com>
2022-09-24 16:36:33 +0530

    String with testcases

---
+++ stringwithtest.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	long long int test,i;
+	cin>>test;
+	while(test--){
+		string str;
+		cin>>str;
+		for(i=0;i<str.size();i++){
+			//code goes here;
+		}
+	}
+	return 0;
+}
