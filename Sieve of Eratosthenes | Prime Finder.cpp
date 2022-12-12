00b0842343ae53048cf5a532e4dea7581e2c0b7f
potent-silence <75715464+potent-silence@users.noreply.github.com>
2022-12-12 15:32:13 +0530

    Sieve of Eratosthenes | Prime Finder
    Code is now made more short and bugs are removed
    If you give input x then you will get the prime number on xth position if all the prime numbers from 2 are arranged in ascending order

--- Sieve of Eratosthenes | Prime Finder.cpp
+++ Sieve of Eratosthenes | Prime Finder.cpp
@@ -6,7 +6,7 @@ int main() {
     vector<int>primes;
     vector<bool> is_prime(n+1, true);
     is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i <= n; i++) {//Here the loop traverses and finds all the prime numbers lower than n
+    for (int i = 2; i <= n; i++) {//Here the loop traverses and finds all the prime numbers lower than n.
         if (is_prime[i]) {
             primes.push_back(i);//Primes are added
             for (int j = i * i; j <= n; j += i)//taking no.s from taken no.s square to the last no.
