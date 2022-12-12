5ecaa83dd449406641823b74a8a6a3f2eb0feaa3
potent-silence <75715464+potent-silence@users.noreply.github.com>
2022-12-12 15:30:30 +0530

    Sieve of Eratosthenes | Prime Finder
    Code is now made more short and bugs are removed

--- Sieve of Eratosthenes | Prime Finder.cpp
+++ Sieve of Eratosthenes | Prime Finder.cpp
@@ -3,20 +3,17 @@ using namespace std;
 
 int main() {
     int n=100000000;
+    vector<int>primes;
     vector<bool> is_prime(n+1, true);
     is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i * i <= n; i++) {
+    for (int i = 2; i <= n; i++) {//Here the loop traverses and finds all the prime numbers lower than n
         if (is_prime[i]) {
+            primes.push_back(i);//Primes are added
             for (int j = i * i; j <= n; j += i)//taking no.s from taken no.s square to the last no.
                 is_prime[j] = false;           //making multiples of the taken no. 0 in declare is_prime array
         }   
     }
-    vector<int>primes;
-    for (int i = 2; i<= n; i++) {
-        if(is_prime[i]){
-            primes.push_back(i);//Primes are added
-        }
-    }
+    
     int test,pos;
     cin>>test;
     while(test--){
