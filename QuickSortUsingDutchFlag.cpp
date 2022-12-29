198ab09e343033bd082652038025aa413ef696c5
potent-silence <75715464+potent-silence@users.noreply.github.com>
2022-12-29 14:42:16 +0530

    Create QuickSortUsingDutchFlag.cpp
    Quick Sort:  https://www.geeksforgeeks.org/quick-sort/
    Dutch Flag: https://www.codingninjas.com/codestudio/guided-paths/data-structures-algorithms/content/118820/offering/1381862

---
+++ QuickSortUsingDutchFlag.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h> 
+
+pair<int,int> partition(vector<int> &arr,int low,int high){
+    int mid1=low,mid2=low,ref=arr[low];//low,mid1,mid2,high(Structure of partition)
+    
+    while(mid2<=high){
+        if(arr[mid2]==ref){
+            mid2++;
+        }
+        else if(arr[mid2]<ref){
+            swap(arr[mid1],arr[mid2]);
+            mid1++,mid2++;
+        }
+        else{
+            swap(arr[mid2],arr[high]);
+            high--;
+        }
+    }
+    return {mid1,mid2};
+}
+
+void quicksort(vector<int> &arr,int low,int high){
+    if(low<high){
+        pair<int,int> mid=partition(arr,low,high);//partition index
+
+        quicksort(arr,low,mid.first-1);//left side
+        quicksort(arr,mid.second,high);//right side
+    }
+}
+
+vector<int> quickSortUsingDutchNationalFlag(vector<int> &arr)
+{
+    // Write your code here.
+    quicksort(arr,0,arr.size()-1);
+    return arr;
+}
